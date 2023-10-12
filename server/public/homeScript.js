d12aa443abd3b5bcfcb7e267f610c41338dcce55
Alex Ramos <alex@ramos.com>
2023-10-12 09:03:12 -0300

    changed the grid system

--- server/public/homeScript.js
+++ server/public/homeScript.js
@@ -16,7 +16,7 @@ $("#btnLogOut").click(logOut);
 
 function enableTaskCreator(){
     const content = `
-    <form class="TaskAddForm">
+    <form class="TaskAddForm col-5 card">
     <div class='card-header pointer bg-white d-flex justify-content-between align-items-center'>
         <svg id="btnAddTask" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -31,10 +31,10 @@ function enableTaskCreator(){
             <input id="txtAddDesc" class="form-control" type="text" placeholder="Description"></p>
         </form>
     `
-    $("#taskCreator").html(content)
+    $("#divBtnAddTask").html(content)
     $("#taskCreator").removeClass("d-flex justify-content-center align-items-center")
     $("#taskCreator").addClass('card')
-    $("#taskCreator").off("click")
+    $("#divBtnAddTask").off("click")
      $("#btnAddTask").click(addTask);
      
 }
@@ -49,8 +49,8 @@ async function  generateTasks(){
             const responseJson = await response.json();
             ///CREATES THE BUTTON TO ENABLE THE FORM TO ADD A NEW TASK
             const formAddTask = `
-            <div id="taskCreator" class='mr-1 mt-3 mb-3 col-sm-4 pointer d-flex justify-content-center align-items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="currentColor" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
+            <div id="taskCreator" class='mr-1 mt-3 mb-3 col-12 col-md-3 col-lg-2 pointer d-flex justify-content-center align-items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-file-earmark-plus" viewBox="0 0 16 16">
                 <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5z"/>
                 <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
                 </svg>
@@ -60,8 +60,8 @@ async function  generateTasks(){
             
             `
             
-             $("#taskDump").append(formAddTask)
-             $("#taskCreator").click(enableTaskCreator);
+             $("#divBtnAddTask").html(formAddTask)
+             $("#divBtnAddTask").click(enableTaskCreator);
              
 
              let colorCard = "";
@@ -79,7 +79,7 @@ async function  generateTasks(){
                 }
                 const content = `
                 
-                <div class='card taskDiv mt-3 mb-3 col-12 col-sm-4'>
+                <div class='card taskDiv text-center mx-2 my-2 col-12 col-md-3 '>
                     
                         <div class='card-header ${colorCard} text-white d-flex justify-content-between align-items-center'>
                             <span class="date">${element.dueDate}</span>
